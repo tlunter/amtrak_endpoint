c13a99b38c0745aa59faaaba62cf6078b057988e
Todd Lunter <tlunter@gmail.com>
2015-09-21 19:47:29 -0400

    Only run heartbeats on production

--- amtrak_endpoint/config/clock.rb
+++ amtrak_endpoint/config/clock.rb
@@ -3,10 +3,12 @@ module Clockwork
 
   every(1.minute, AmtrakEndpoint::EnqueueTimes)
 
-  every(1.minute, 'clock.heartbeat') do
-    client = Dogapi::Client.new(DATA_DOG_API_KEY)
-    client.service_check('clock.heartbeat', 'docker', 0)
-  end
+  if ENV['RACK_ENV'] == 'production'
+    every(1.minute, 'clock.heartbeat') do
+      client = Dogapi::Client.new(DATA_DOG_API_KEY)
+      client.service_check('clock.heartbeat', 'docker', 0)
+    end
 
-  every(1.minute, AmtrakEndpoint::WorkerHeartbeat)
+    every(1.minute, AmtrakEndpoint::WorkerHeartbeat)
+  end
 end
