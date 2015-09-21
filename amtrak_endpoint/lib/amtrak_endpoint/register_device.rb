b5f869dcf36703d1e85e2c4bddb1c6902188b828
Todd Lunter <tlunter@gmail.com>
2015-09-21 19:52:24 -0400

    Register a device for a train route only and don't deal with UUIDs

--- amtrak_endpoint/lib/amtrak_endpoint/register_device.rb
+++ amtrak_endpoint/lib/amtrak_endpoint/register_device.rb
@@ -1,19 +1,13 @@
 module AmtrakEndpoint
   class RegisterDevice < Base
-    post %r{^/register/android/(?<device_id>[^/]+)/(?<train_number>[^\.]+)\.json} do
+    post %r{^/register/android/(?<device_id>[^\.]+)\.json} do
       headers['Content-Type'] = 'application/json'
 
       train_routes = JSON.parse(request.body.tap(&:rewind).read)
 
       train_routes.each do |train_route|
-        uuid = UUIDTools::UUID.random_create
-        device = AmtrakEndpoint::Device.new(uuid)
+        device = AmtrakEndpoint::Device.new(params['device_id'])
         device.type = 'android'
-        device.type_params.clear
-        device.type_params.update(
-          train_number: params['train_number'],
-          device_id: params['device_id']
-        )
 
         AmtrakEndpoint::TrainRoute.new(
           from: train_route["from"], to: train_route["to"]
